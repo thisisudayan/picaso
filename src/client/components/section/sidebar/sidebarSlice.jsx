97143ecbe1813b3d9652efdec2e4c9ca23ddcaa3
Maruf Bin Solaiman <0909maruf@gmail.com>
2024-07-31 16:56:43 +0600

    image card done.

--- src/client/components/section/sidebar/sidebarSlice.jsx
+++ src/client/components/section/sidebar/sidebarSlice.jsx
@@ -3,43 +3,50 @@ import { createSlice } from '@reduxjs/toolkit'
 const sidebarSlice = createSlice({
   name: 'sidebar',
   initialState: {
-    conversationKey:null,
-    conversations:[
+    conversationKey: null,
+    conversations: [
       {
-        id:'dfgfhg',
-        title:"Create a beautiful potrait",
+        id: 'dfgfhg',
+        title: "Create a beautiful potrait",
         messages: [
           {
-            id:'sfadagag',
-            attachments:[
+            id: 'eig840d',
+            attachments: [],
+            question: true,
+            body: 'generate some picture related to nature'
+          },
+          {
+            id: '346jsd6',
+            attachments: [
               "https://picsum.photos/200/300",
               "https://picsum.photos/200/300",
               "https://picsum.photos/200/300",
               "https://picsum.photos/200/300",
               "https://picsum.photos/200/300",
+              "https://picsum.photos/200/300",
+              "https://picsum.photos/200/300",
+
             ],
             question: false,
-            body: 'Hello World'
+            body: 'here are some nature related pictures for you'
           }
         ]
       }
     ]
   },
   reducers: {
-    selectConversation: ((state,action) => {
-        state.conversationKey = action.payload
+    selectConversation: ((state, action) => {
+      state.conversationKey = action.payload
     }),
     setConversation: ((state, action) => {
-      console.log(action.payload.id)
-      console.log(state.conversations[0].id)
       state.conversations = state.conversations.filter((item) => {
         return item.id === action.payload.id
 
       })
-     
+
     })
   }
 })
 
-export const { selectConversation,setConversation} = sidebarSlice.actions
+export const { selectConversation, setConversation } = sidebarSlice.actions
 export default sidebarSlice.reducer;
